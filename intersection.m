bad21cf1fc723386c7f165b9404be8036e59eda1
jb <43890665+0xBAMA@users.noreply.github.com>
2024-04-17 00:01:21 -0500

    Add files via upload

---
+++ intersection.m
@@ -0,0 +1,80 @@
+#! octave-interpreter-name -qf
+pkg load image
+pkg load statistics
+
+xDimension = 100;
+yDimension = 500;
+padding = 25;
+minWavelength = 400; # units of nm
+maxWavelength = 700; # units of nm
+numWavelengths = 3;
+
+# the buffers we're writing to
+global fieldImageWavelengths;
+
+# write some stuff to the fieldImage
+function drawLine( start, stop, wavelengthBin )
+
+	global fieldImageWavelengths;
+	offset = stop - start; # normalizing the offset vector
+	length = sqrt( offset( 1 ) * offset( 1 ) + offset( 2 ) * offset( 2 ) );
+	offset = ( offset ) ./ length;
+
+	for i = 1:length
+		# fraction = i / length;
+		writeLoc = start + i * offset;
+		fieldImageWavelengths( int32( writeLoc( 1 ) ), int32( writeLoc( 2 ) ), wavelengthBin ) += 1;
+	endfor
+end
+
+function result = remap( value, low1, high1, low2, high2 )
+	result = low2 + ( value - low1 ) * ( high2 - low2 ) / ( high1 - low1 );
+end
+
+# clear the sim field
+fieldImageWavelengths = zeros( xDimension, yDimension, numWavelengths );
+
+# replacing the loop below with a function that like, shoots a ray - shoot( sourcePoint, direction, wavelength )
+
+numSteps = 100
+for steps = 1:numSteps
+	# run the simulation for some number of rays
+	for ray = 1:100
+
+		# what wavelength am I? 400nm to 700nm, visible light spectra
+		rayWavelength = unifrnd( minWavelength, maxWavelength );
+		wavelengthBin = int32( floor( remap( rayWavelength, minWavelength, maxWavelength, 1, numWavelengths + 1 ) ) );
+
+		# what does a ray do? Starting on the far left hand side, at a random point along the height
+		point = [ unifrnd( padding, xDimension - padding ) 0 ];
+		drawLine( point, point + [ 0 500 ], wavelengthBin );
+
+		# Ok, now they refract off the first surface, with wavelength dependent IoR
+
+		# and then...
+
+	end
+	printf( "\rStep %d of %d     ", steps, numSteps );
+end
+
+# create the output image
+fieldImage = zeros( xDimension, yDimension, 3 );
+
+# resolve the field image, from the wavelength bin histograms - some kind of vector mixing... yeah. tbd
+# first things first, just 3 channel, matching
+
+# determine cell with max total flux to scale the overall image accordingly
+maxFlux = max( fieldImageWavelengths( : ) );
+
+# need to compute the weighted output color, histogram times the corresponding colors for each frequency bin
+
+fieldImage = fieldImageWavelengths ./ maxFlux;
+
+# elementwise max of wavelengths bins, across the image... take the max of that... sum of that?
+
+image( fieldImage );
+
+# save out the fieldImage
+# imwrite( fieldImage, 'test.png', 'png' );
+printf( "Image Written.\n" );
+# close all
---
+++ utilsTest.m
@@ -0,0 +1,50 @@
+#! octave-interpreter-name -qf
+pkg load image
+pkg load statistics
+
+xDimension = 1000;
+yDimension = 1000;
+channels = 3;
+
+# the buffer we're writing to
+global fieldImage;
+
+# write some stuff to the fieldImage
+function drawLine( start, startColor, stop, stopColor )
+	global fieldImage;
+	offset = stop - start; # normalizing the offset vector
+	length = sqrt( offset( 1 ) * offset( 1 ) + offset( 2 ) * offset( 2 ) );
+	offset = ( offset ) ./ length;
+
+	for i = 1:length
+		writeLoc = start + i * offset;
+
+		fraction = i / length;
+		interpolatedColor = ( startColor .* ( 1 - fraction ) ) +  ( stopColor .* fraction );
+
+		fieldImage( int32( writeLoc( 1 ) ), int32( writeLoc( 2 ) ), 1 ) = interpolatedColor( 1 );
+		fieldImage( int32( writeLoc( 1 ) ), int32( writeLoc( 2 ) ), 2 ) = interpolatedColor( 2 );
+		fieldImage( int32( writeLoc( 1 ) ), int32( writeLoc( 2 ) ), 3 ) = interpolatedColor( 3 );
+	endfor
+end
+
+# clear the image
+fieldImage = zeros( xDimension, yDimension, channels );
+
+# drawing some random lines
+numLines = 1000
+for i = 1:numLines
+	points = unifrnd( 10, 990, 2, 2 );
+	drawLine(
+		points( 1, : ), [ 0, 0, 1 ],
+		points( 2, : ), [ 0, 1, 1 ]
+	);
+	printf( "\r%d / %d          ", i, numLines );
+endfor
+
+imshow( fieldImage );
+
+# save out the fieldImage
+# imwrite( fieldImage, 'test.png', 'png' );
+printf( "Image Written.\n" );
+# close all
